7dcc118e45604047328a2137f429556810dad3f4
jlramos <Asuka@eva02>
2022-03-23 15:21:03 -0300

    first commit

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .lineSpacing(4.0)
+                .font(.footnote)
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+        Text("89")
+        HStack{
+            Text("1")
+            Slider(value: .constant(50), in: 1.0...100.0)
+            Text("100")
+        }
+            Button("Hit me") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
+
